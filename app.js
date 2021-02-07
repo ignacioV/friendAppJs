177ffadf473c65f5d7490d4d9ddd99c3d4e2bc11
ignelio <ltuwis@gmail.com>
2021-02-07 20:31:26 +0200

    added static file serving for express, npm start command

--- app.js
+++ app.js
@@ -2,13 +2,15 @@ const express = require('express')
 const path = require('path')
 
 const app = express()
+app.use('/src', express.static('src'))
 
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirnamem, '/index.html'))
+  res.sendFile(path.join(__dirname, '/index.html'))
 })
 
 app.listen(3000, () => {
   console.log("listening on port 3000");
+  console.log("dirrname", __dirname);
 })
 
 // const http = require('http')
