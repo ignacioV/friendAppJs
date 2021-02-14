36a32b5aaaa9a78b7ca8e8da8d6941770f93806f
ignelio <ltuwis@gmail.com>
2021-02-14 16:44:43 +0200

    remove comments

--- app.js
+++ app.js
@@ -81,27 +81,3 @@ app.post('/friends', (req, res) => {
 app.listen(PORT, () => {
   console.log(`listening on port ${PORT}`);
 })
-
-// const http = require('http')
-// const fs = require('fs')
-
-// const hostname = '127.0.0.1';
-// const port = 3000;
-
-// const server = http.createServer((req, res) => {
-// //     res.statusCode = 200;
-// //   res.setHeader('Content-Type', 'text/plain');
-// //   res.end('Hello World');
-
-//   res.writeHead(200, { 'content-type': 'text/html' })
-//   fs.createReadStream('index.html').pipe(res)
-// })
-
-// server.listen(port, hostname, () => {
-//     console.log(`Server running at http://${hostname}:${port}/`);
-//   });
-
-// // export function addToFile() {
-// //   console.log("addin to file");
-// //   fs.appendFile("../data/MOCK_DATA.json", "lol", err => {console.error(err)})   
-// // }
--- src/app/service.js
+++ src/app/service.js
@@ -2,16 +2,10 @@ import { makeFriendsList } from "./list/friends-list-maker.js";
 import { saveNewFriend } from "./friend-forms/new-friend-form.js"
 import { handleNewFriendFormVisibility } from "./friend-forms/new-friend-form-visibility.js"
 
-// import { addToFile } from "../../server/app.js"
-
 export function app() {
     console.log("running app")
 
     makeFriendsList();
     saveNewFriend();
     handleNewFriendFormVisibility();
-
-    // document.getElementById("append-to-file-btn").onclick = e => {
-    //     addToFile();
-    // }
 }
