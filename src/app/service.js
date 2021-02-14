940da0f8dff06b2d93984a7d746380c0f050f61d
ignelio <ltuwis@gmail.com>
2021-02-14 21:52:45 +0200

    add remove button icons, toggling functionality

--- src/app/service.js
+++ src/app/service.js
@@ -1,10 +1,11 @@
 import { getFriendsList } from "./list/friends-list.js";
 import { handleNewFriendFormVisibility } from "./friend-forms/new-friend-form-visibility.js"
-
+import { toggleRemoval } from "./list/remove-friend.js"
 export function app() {
     console.log("running app")
 
     getFriendsList();
     // saveNewFriend();
     handleNewFriendFormVisibility();
+    toggleRemoval()
 }
